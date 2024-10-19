48cfbde34ec02dcd2fad9ed437493853f167c708
Titux Metal (DEV) <github@lgdweb.fr>
2024-10-19 21:04:38 +0200

    refactor: remove task-related features and migrations
    - Deleted all task-related database migrations and schema definitions.
    - Removed task seeding logic from `prisma/seed.ts`.
    - Eliminated task-related actions and components, including `TaskForm`, `task.actions.ts`, and UI components.
    - Updated `src/actions/index.ts` to remove task actions.
    - Removed task-related services and schemas.

--- prisma/migrations/20241019160617_init/migration.sql
+++ prisma/migrations/20241019160617_init/migration.sql
@@ -8,8 +8,7 @@ CREATE TABLE "User" (
 CREATE TABLE "Session" (
     "id" TEXT NOT NULL PRIMARY KEY,
     "userId" TEXT NOT NULL,
-    "activeExpires" DATETIME NOT NULL,
-    "idleExpires" DATETIME NOT NULL,
+    "expiresAt" DATETIME NOT NULL,
     CONSTRAINT "Session_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
 );
 
--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,9 +1,7 @@
 import { auth } from './auth.actions'
-import { task } from './task.actions'
 
 export const server = {
   signup: auth.signup,
   login: auth.login,
-  logout: auth.logout,
-  createTask: task.create
+  logout: auth.logout
 }
